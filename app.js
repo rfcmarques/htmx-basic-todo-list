5746a2529d44dfbf6783dbb739c5141b2f8ffc29
Rui Marques <a.marques1967@gmail.com>
2024-04-05 00:21:06 +0100

    Removed unnecessary code

--- app.js
+++ app.js
@@ -28,7 +28,7 @@ app.get("/", (req, res) => {
             hx-target="#goals"
             hx-swap="outerHTML"
             hx-select="#goals"
-            hx-on:htmx:after-request="document.querySelector('form').reset()"
+            hx-on::after-request="this.reset()"
           >
             <div>
               <label htmlFor="goal">Goal</label>
